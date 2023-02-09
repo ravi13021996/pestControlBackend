b2e4732b20f34227be8e989537861e00b6171ab1
ravikant <ravidwivedi1133@gmail.com>
2023-02-09 17:15:57 +0530

    render file

--- routes/enquiryRoute.js
+++ routes/enquiryRoute.js
@@ -1,7 +1,7 @@
 const addEnquiryData = require("../services/enquiryServices")
 
 const router = require("express").Router()
-
+const path= require("path")
 router.post('/enquiry', (req, res) => {
     console.log(req.body, "body")
     addEnquiryData(req.body).then((response) => {
@@ -13,7 +13,10 @@ router.post('/enquiry', (req, res) => {
 })
 
 router.get('/',(req,res)=>{
-    res.render(<h1>hiii</h1>)
+    // console.log(__dirname+"../","__dirname")
+    let path2 =path.join(__dirname,'../'+'/index.html')
+    console.log(path2,"path")
+    res.sendFile(path2)
 })
 
 module.exports = router
